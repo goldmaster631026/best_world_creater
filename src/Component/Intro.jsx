c0da6ff9e45b8f2067e044baa9b2135710a290a2
goldmaster631026 <goldmaster631026@gmail.com>
2024-11-12 21:39:50 +1000

    update

--- src/Component/Intro.jsx
+++ src/Component/Intro.jsx
@@ -240,7 +240,7 @@ export default function Intro() {
                 >
                   <Typewriter
                     words={[
-                      'A Full Stack || Blockchain Web3 Developer.',
+                      'A Full Stack || AI || Web3 Developer.',
                       'My primary development languages for creating impressive projects are React, Tailwind , MUI, Python, Solidity and Rust.',
                     ]}
                     loop={false}
