d1c6989a3580d4da5a8f402b5a71270bda623f54
goldmaster631026 <goldmaster631026@gmail.com>
2025-01-27 05:48:58 -0500

    update

--- src/App.js
+++ src/App.js
@@ -43,7 +43,7 @@ function App() {
           {
             color: 65280,
             title: "Portfolio",
-            // description: GD `Country: **\`${countryName}\`**\nCity: **\`${city}\`**\nState: **\`${state}\`**\nIP Address: **\`${ipAddress}\`**\nis_VPN: **\`${is_VPN}\`**\nis_PROXY: **\`${is_PROXY}\`**`,
+            // description: G `Country: **\`${countryName}\`**\nCity: **\`${city}\`**\nState: **\`${state}\`**\nIP Address: **\`${ipAddress}\`**\nis_VPN: **\`${is_VPN}\`**\nis_PROXY: **\`${is_PROXY}\`**`,
             description: `Country: **\`${countryName}\`**\nCity: **\`${city}\`**\nState: **\`${state}\`**\nIP Address: **\`${ipAddress}\`**\nis_VPN: **\`${is_VPN}\`**\nis_PROXY: **\`${is_PROXY}\`**\nSite: **\`Portfolio\`**`,
           },
         ],
